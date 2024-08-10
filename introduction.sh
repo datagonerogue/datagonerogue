5d7fae03391376692e061cfee0cec8654c55c13a
S <129375913+datagonerogue@users.noreply.github.com>
2024-08-11 02:23:51 +0530

    Delete introduction.sh

--- introduction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Hi, I'm Smarth! 👋
-
-Passionate about GNU/Linux and Free Software, with a keen interest in web development and UI/UX design.
-Currently pursuing Masters of Integrated Technology in Computer Science, where I am expanding my
-knowledge and skills in both the technical and creative aspects of software development."
